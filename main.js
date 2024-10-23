5bb73565e137169b7ad8424c4a0dff45dffad5db
mLascurain <matiaslascurain04@gmail.com>
2024-10-23 00:55:50 -0300

    added enter keydown function and comments

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
+// Agarro la api key del archivo
 import apiKey from "./apikey.js";
 const API_KEY = apiKey;
+
+// Guardo el elemento input en una variable
+const inputUser = document.querySelector("input");
+
+// Obtengo el valor del input y le mando la url a la funcion llamarAPI()
 function obtenerValor() {
   let valorInput = document.querySelector("input").value;
   const url = `https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${valorInput}`;
   llamarAPI(url);
 }
 
+// Cuando le doy click al boton buscar se ejecuta la busqueda
 const searchButton = document.querySelector("button");
 searchButton.addEventListener("click", () => {
   obtenerValor();
+  inputUser.value = "";
 });
 
+// Cuando se toca la tecla enter se ejecuta la busqueda
+inputUser.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    obtenerValor();
+    inputUser.value = "";
+  }
+});
+
+// Hace una consulta a la api y devuelve los valores que obtiene de ella
 const llamarAPI = (url) => {
   fetch(url, { mode: "cors" })
     .then(function (response) {
@@ -21,6 +38,7 @@ const llamarAPI = (url) => {
     });
 };
 
+// Modifica elementos del DOM para hacer visible para el usuario el gif
 const mostrarGif = (response) => {
   const container = document.querySelector(".img-container");
   const img = document.createElement("img");
